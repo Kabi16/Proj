6d4fe8583b51e14632d90a535d4f3766cd750d0a
Kabi16 <86357347+Kabi16@users.noreply.github.com>
2022-07-22 14:12:01 +0530

    Update Build.cmd

--- Build.cmd
+++ Build.cmd
@@ -4,8 +4,8 @@
 @FOR %%A IN (%*) DO @IF "%%A"=="Debug" @SET MODE=Debug
 
 REM Provide solution path and sonar key for the service
-@SET SOLU="dotnetHelloWorld.sln"
-@SET SOLUPATH="."
+@SET SOLU="C:\dotnethelloworld\dotnetHelloWorld.sln"
+@SET SOLUPATH="C:\dotnethelloworld\HelloWorld"
 
 
 @SET MSBUILDDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin
