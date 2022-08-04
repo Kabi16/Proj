e6f9f2698919b8a75f7951fe921ae143b89cbe3f
Kabi16 <86357347+Kabi16@users.noreply.github.com>
2022-08-04 17:37:26 +0530

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,9 +1,9 @@
-﻿Expand-Archive -LiteralPath "C:\Workspace\BuildJob\src\dotnetHelloWorld\bin\Release\package.zip" -DestinationPath "C:\DeployLogs"
-Move-Item –Path C:\DeployLogs\Content\C_C\Workspace\BuildJob\src\dotnetHelloWorld\obj\Release\netcoreapp2.1\PubTmp\Out -Destination C:\DeploySite
-$APPPOOL_NAME="DeployPool"
-$SITE_NAME="Deploy"
-$PORT=5000
-$PATH="C:\DeploySite\Out"
+﻿Expand-Archive -LiteralPath "C:\Workspace\BuildPipeline\src\dotnetHelloWorld\bin\Release\package.zip" -DestinationPath "C:\DeployLogs2"
+Move-Item –Path C:\DeployLogs2\Content\C_C\Workspace\BuildJob\src\dotnetHelloWorld\obj\Release\netcoreapp2.1\PubTmp\Out -Destination C:\DeploySite2
+$APPPOOL_NAME="DeployPool2"
+$SITE_NAME="Deploy2"
+$PORT=5001
+$PATH="C:\DeploySite2\Out"
 New-WebAppPool -Force -Name "$APPPOOL_NAME"
 Set-ItemProperty -Path IIS:\AppPools\$APPPOOL_NAME managedRuntimeVersion "v4.0"
 New-Item IIS:\Sites\$SITE_NAME -physicalPath $PATH -bindings @{protocol="http";bindingInformation="*:${PORT}:"}
